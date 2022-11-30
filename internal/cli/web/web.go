baa3b55676ed95aef09363150f043a836e9d508c
cong.dang <cong.dang569@gmail.com>
2022-11-30 22:54:11 +0700

    fix comments

--- internal/cli/web/web.go
+++ internal/cli/web/web.go
@@ -126,7 +126,7 @@ func (a *router) productDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	comments, err := a.App.Comments.Slug(slug)
+	comments, err := a.App.Comments.Slug(fmt.Sprintf("/blog/%s", slug))
 	if err != nil {
 		comments = []*models.Comment{}
 	}
@@ -1000,8 +1000,14 @@ func (a *router) blogDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	comments, err := a.App.Comments.Slug(fmt.Sprintf("/real-estate/%s", slug))
+	if err != nil {
+		comments = []*models.Comment{}
+	}
+
 	a.render(w, r, "blog.detail.page.html", &templateData{
-		Post: post,
+		Post:     post,
+		Comments: comments,
 	})
 }
 
